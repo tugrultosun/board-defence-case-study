3b73f3b1efc62393bd0f2644f8013c986a082407
Tuğrul Tosun <ttosun350@gmail.com>
2025-06-03 15:15:17 +0300

    adds enemy data model and its scriptable settings

---
+++ Assets/Scripts/Enemy/EnemyDataModel.cs
@@ -0,0 +1,9 @@
+namespace Enemy
+{
+    [System.Serializable]
+    public class EnemyDataModel 
+    {
+        public int health;
+        public float speed;
+    }
+}
---
+++ Assets/Scripts/Settings/Enemy Settings.cs
@@ -0,0 +1,13 @@
+using Enemy;
+using UnityEngine;
+
+namespace Settings
+{
+    [CreateAssetMenu(fileName = "EnemySettings", menuName = "Scriptable Objects/EnemySettings", order = 1)]
+    public class EnemySettings : ScriptableObject
+    {
+        public EnemyDataModel enemyData1;
+        public EnemyDataModel enemyData2;
+        public EnemyDataModel enemyData3;
+    }
+}
