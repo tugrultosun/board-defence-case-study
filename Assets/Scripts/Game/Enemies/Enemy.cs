a9d544c014dd505c67060c66cb71324a1caf0a93
Tuğrul Tosun <ttosun350@gmail.com>
2025-06-05 14:17:07 +0300

    adds new sorting layers and hp text for enemy

--- Assets/Scripts/Game/Enemies/Enemy.cs
+++ Assets/Scripts/Game/Enemies/Enemy.cs
@@ -2,6 +2,7 @@ using System;
 using Game.Board;
 using Lean.Pool;
 using Managers;
+using TMPro;
 using UnityEngine;
 
 namespace Game.Enemies
@@ -9,6 +10,8 @@ namespace Game.Enemies
     public class Enemy : MonoBehaviour , IEnemy
     {
         [SerializeField] private SpriteRenderer spriteRenderer;
+        
+        [SerializeField] private TextMeshPro hpText; 
         public EnemyType EnemyType { get; set; }
         public int Health { get; set; }
         public float Speed { get; set; }
@@ -21,11 +24,13 @@ namespace Game.Enemies
             Health = enemyDataModel.health;
             Speed = enemyDataModel.speed;
             CanMove = true;// can be changed based on events or other things
+            hpText.SetText(Health.ToString());
         }
 
         public void ApplyDamage(int damage)
         {
             Health -= damage;
+            hpText.SetText(Health.ToString());
             Debug.Log("defender attacked, enemy hp is now:" + Health);
             if (Health <= 0)
             {
